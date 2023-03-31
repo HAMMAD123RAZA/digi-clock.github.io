7fb38aca6ab32dd5110e27166313fdc2d85d8ab9
Hammad Raza <01hammadraza@gmail.com>
2023-03-31 18:08:18 +0500

    made  reponsive

--- js.js
+++ js.js
@@ -1,48 +1,22 @@
-// const display = () => {
-//     let time = new Date()
-//     let hr = time.getHours()
-//     let min = time.getMinutes()
-//     let sec = time.getSeconds()
-//     let session = document.getElementById('ses')
-
-//     if (hr >= 12) {
-//         session.innerHTML = 'pm'
-//     }
-//     else {
-//         session.innerHTML = 'am'
-//     }
-//     // if(hr>12){
-//     // hr=hr-12
-//     // }
-//     let hour = document.getElementById('hour').innerHTML = hr;
-//     let minute = document.getElementById('min').innerHTML = min;
-//     let second = document.getElementById('sec').innerHTML = sec;
-
-// }
-// setInterval(display, 10)
-
 const display = () => {
-    let time = new Date();
-    let date = time.toLocaleDateString(); // get the date as a string
-    let hr = time.getHours();
-    let min = time.getMinutes();
-    let sec = time.getSeconds();
-    let session = document.getElementById('ses');
-  
-    if (hr >= 12) {
-      session.innerHTML = 'pm';
-    } else {
-      session.innerHTML = 'am';
-    }
-    // if(hr>12){
-    // hr=hr-12
-    // }
-    let hour = document.getElementById('hour').innerHTML = hr;
-    let minute = document.getElementById('min').innerHTML = min;
-    let second = document.getElementById('sec').innerHTML = sec;
-    let dateSpan = document.getElementById('date').innerHTML = date;
-     // set the date in the HTML element
-  };
-  
-  setInterval(display, 10);
-  
+let time=new Date()
+let hr=time.getHours()
+let min=time.getMinutes()
+let sec=time.getSeconds()
+let session=document.getElementById('ses')
+
+if(hr>=12){
+session.innerHTML='pm'
+}
+else{
+    session.innerHTML='am'
+}
+// if(hr>12){
+// hr=hr-12
+// }
+let hour=document.getElementById('hour').innerHTML=hr;
+let minute=document.getElementById('min').innerHTML=min;
+let second=document.getElementById('sec').innerHTML=sec;
+
+}
+setInterval(display,10)
