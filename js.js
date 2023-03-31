a8fc86b5d465ed370806bd98ece52f7db7a38f23
Hammad Raza <01hammadraza@gmail.com>
2023-03-31 15:45:45 +0500

    made reponsive

--- js.js
+++ js.js
@@ -1,22 +1,48 @@
-const display = () => {
-let time=new Date()
-let hr=time.getHours()
-let min=time.getMinutes()
-let sec=time.getSeconds()
-let session=document.getElementById('ses')
+// const display = () => {
+//     let time = new Date()
+//     let hr = time.getHours()
+//     let min = time.getMinutes()
+//     let sec = time.getSeconds()
+//     let session = document.getElementById('ses')
 
-if(hr>=12){
-session.innerHTML='pm'
-}
-else{
-    session.innerHTML='am'
-}
-// if(hr>12){
-// hr=hr-12
+//     if (hr >= 12) {
+//         session.innerHTML = 'pm'
+//     }
+//     else {
+//         session.innerHTML = 'am'
+//     }
+//     // if(hr>12){
+//     // hr=hr-12
+//     // }
+//     let hour = document.getElementById('hour').innerHTML = hr;
+//     let minute = document.getElementById('min').innerHTML = min;
+//     let second = document.getElementById('sec').innerHTML = sec;
+
 // }
-let hour=document.getElementById('hour').innerHTML=hr;
-let minute=document.getElementById('min').innerHTML=min;
-let second=document.getElementById('sec').innerHTML=sec;
+// setInterval(display, 10)
 
-}
-setInterval(display,10)
+const display = () => {
+    let time = new Date();
+    let date = time.toLocaleDateString(); // get the date as a string
+    let hr = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    let session = document.getElementById('ses');
+  
+    if (hr >= 12) {
+      session.innerHTML = 'pm';
+    } else {
+      session.innerHTML = 'am';
+    }
+    // if(hr>12){
+    // hr=hr-12
+    // }
+    let hour = document.getElementById('hour').innerHTML = hr;
+    let minute = document.getElementById('min').innerHTML = min;
+    let second = document.getElementById('sec').innerHTML = sec;
+    let dateSpan = document.getElementById('date');
+    dateSpan.innerHTML = date; // set the date in the HTML element
+  };
+  
+  setInterval(display, 10);
+  
